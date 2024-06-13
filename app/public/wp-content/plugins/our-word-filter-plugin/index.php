e7265aa5336cb7eb50d83f883baf1f6dc0a778d4
Manav Rajani <83126261+Manav2626@users.noreply.github.com>
2024-06-13 21:25:08 +0530

    Started new Plugin to show on Admin sidebar List, and submenus

--- app/public/wp-content/plugins/our-word-filter-plugin/index.php
+++ app/public/wp-content/plugins/our-word-filter-plugin/index.php
@@ -11,6 +11,28 @@
 if(!defined('ABSPATH')) exit; // Exits if someone tries to directly access Plugin
 
 
+
+
 class OurWordFilterPlugin{
-    
+    function __construct()
+    {
+        add_action('admin_menu', array($this, 'ourMenu'));
+    }
+
+    function ourMenu(){
+        add_menu_page('Word Filter','Word Filter','manage_options','ourwordfilter',array($this, 'wordFilterPage'),'dashicons-smiley','100');
+        add_submenu_page('ourwordfilter', ' Words To Filter', 'Words List', 'manage_options', 'ourwordfilter', array($this, 'wordFilterPage'));
+        add_submenu_page('ourwordfilter', ' Word Filter Options', 'Options', 'manage_options', 'word-filter-option', array($this, 'optionSubPage'));
+    }
+    function wordFilterPage(){ ?>
+    Hello World
+    <?php }
+    function optionSubPage(){ ?>
+    Hello World Subsssss
+    <?php }
 }
+
+$OurWordFilterPlugin=new OurWordFilterPlugin();
+
+
+
